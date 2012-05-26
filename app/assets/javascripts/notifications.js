e02f8a6640ab0df7e15f4266f18c3725d115e36b
Martin "quill18" Glaude <quill18@towerdive.com>
2012-05-26 13:18:46 -0400

    Added title-bar notification of notifications

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -12,5 +12,14 @@ $(function() {
 });
 
 function checkNotifications() {
-	$("#toolbar_notifications").load( $("#toolbar_notifications").data("ajax-url") );
+	$("#toolbar_notifications").load( $("#toolbar_notifications").data("ajax-url"), function () {
+		var num_notifications = $("#new_notifications").html();
+
+		if(num_notifications != null) {
+			document.title = "["+num_notifications+"] Fish Tank Commander";
+		}
+		else {
+			document.title = "Fish Tank Commander";
+		}
+	} );
 }
